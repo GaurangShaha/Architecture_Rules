f4615e7c4a14abaf1e3c13c34df9ec023be5aec6
Gaurang <gaurang.shaha@gmail.com>
2025-02-15 19:09:20 +0530

    Added custom detekt rules

---
+++ src/test/kotlin/com/flea/market/architecture/rules/ViewModelShouldNotHavePublicPropertyTest.kt
@@ -0,0 +1,34 @@
+package com.flea.market.architecture.rules
+
+import io.gitlab.arturbosch.detekt.test.TestConfig
+import io.gitlab.arturbosch.detekt.test.lint
+import org.junit.jupiter.api.Test
+
+class ViewModelShouldNotHavePublicPropertyTest {
+    private val testConfig = TestConfig()
+
+    private val rule = ViewModelShouldNotHavePublicProperty(testConfig)
+
+    @Test
+    fun `passes when viewmodel does not have public property`() {
+        val code = """
+            class NewsViewModel:ViewModelContract<ProductDetailsIntent, ProductDetailsUiState>(Loading){
+                override fun processIntent(){}
+            }
+""".trimIndent()
+
+        assert(rule.lint(code).isEmpty())
+    }
+
+    @Test
+    fun `error when viewmodel has public property`() {
+        val code = """
+            class NewsViewModel(val id: Int):ViewModelContract<ProductDetailsIntent, ProductDetailsUiState>(Loading){
+                val name = "Harry"
+                override fun processIntent(){}
+            }
+""".trimIndent()
+
+        assert(rule.lint(code).isNotEmpty())
+    }
+}
